c36e8e14d3998cb02f9842c132a8d076413b4527
picodegallo <pocodegallo@gmail.com>
2013-06-20 18:10:38 -0400

    refactored with generic attributes - DAAAAMN

--- student.rb
+++ student.rb
@@ -3,8 +3,6 @@ require 'pry'
 
 class Student
 
-  attr_accessor :name, :id, :bio, :tagline
-
   DB = SQLite3::Database.new( "students.db" )
 
   ATTRIBUTES = {
@@ -14,8 +12,11 @@ class Student
     "bio" => "TEXT"
   }
 
+  ATTRIBUTES.each do |key, value|
+    attr_accessor key.to_sym
+  end
+
   def self.create_table
-    # DB.execute("CREATE TABLE students (#{('?, '*ATTRIBUTES.size)[0..-3]});",ATTRIBUTES)
     DB.execute("CREATE TABLE students (
       #{ATTRIBUTES.collect {|key, value| 
         key + " " + value
@@ -34,12 +35,14 @@ class Student
 
   def save
     if @id #already in database
-      DB.execute("UPDATE students SET 
-        name = '#{@name}', id = #{@id} WHERE 
-        id = #{@id}
-        ;")
+       ATTRIBUTES.each do |key, value|
+        DB.execute("UPDATE students SET #{key} = ? WHERE id = ?;", [self.send(key), @id])
+      end
     else #if it isn't
-      DB.execute("INSERT INTO students(name, bio, tagline) VALUES (?,?,?);",[@name, @bio, @tagline]) 
+       DB.execute("INSERT INTO students(#{ATTRIBUTES.keys[1..-1].join(",")}) VALUES 
+         (#{('?,' * (ATTRIBUTES.size - 1))[0..-2]});", 
+         ATTRIBUTES.keys[1..-1].collect{|key| self.send(key)}) 
+    
       max_id = DB.execute("SELECT max(id) FROM students;")
       @id = max_id.flatten.first
     end
@@ -47,17 +50,15 @@ class Student
 
   def self.make_student_from_record(record)
     student = Student.new.tap do |s|
-      s.id = record[0]
-      s.name = record[1] 
-      s.bio = record[2] 
-      s.tagline = record[3]
+      ATTRIBUTES.keys.each_with_index do |key, index| 
+        s.send(key + '=', record[index])
+      end
     end
   end
 
   def self.find_by_name(name)
     record = DB.execute("SELECT * FROM students WHERE name = '#{name}';").first
     make_student_from_record(record)
-    #Student.new.tap{|s| s.name = record.first[1]}
   end
 
   def self.find_by_bio(bio)
@@ -71,13 +72,12 @@ class Student
   end
 
   def self.all
-    DB.execute("SELECT * FROM students;").collect{|record| Student.new.tap{|s| s.name = record[1]}}
+    DB.execute("SELECT * FROM students;").collect{|record| make_student_from_record(record)}
   end
 
   def self.find(finder_id)
     record = DB.execute("SELECT * FROM students WHERE id = #{finder_id};").first
     make_student_from_record(record)
-    #Student.new.tap{|s| s.id = record.first.first ; s.name = record.first.last}
   end
 
   def self.where(query)
@@ -91,6 +91,7 @@ class Student
 
 
 end
+
 
 
 
